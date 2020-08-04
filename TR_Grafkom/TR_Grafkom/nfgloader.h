6f0c102bd405bb4b9c961df5b361c0256d21e260
Bublaaa <66330115+Bublaaa@users.noreply.github.com>
2020-08-04 15:41:40 +0700

    Update nfgloader.h

--- TR_Grafkom/TR_Grafkom/nfgloader.h
+++ TR_Grafkom/TR_Grafkom/nfgloader.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "vertex.h"
 
-int loadnfg(const char* path)
+int load(const char* path)
 {
 	float baris, nrmx, nrmy, nrmz, binrmx, binrmy, binrmz, tgtx, tgty, tgtz;
 	int nvertices, nrindices;
@@ -10,23 +10,23 @@ int loadnfg(const char* path)
 	fopen_s(&NFG, path, "r");
 	if (!NFG)
 	{
-		printf("Impossible to open the file !\n");
+		printf("Can't open the file !\n");
 		return 0;
 	}
 
 	fscanf_s(NFG, "NrVertices:%d", &nvertices);
-	for (int a = 1;a <= nvertices;a++) 
+	for (int a = 0; a < nvertices; a++)
 	{
 		//0. pos: [0.134000, 1.020300, -0.083900] ; norm: [0.662725, 0.317712, -0.678126] ; binorm: [0.014559, 0.899903, 0.435847] ; tgt: [-0.748721, 0.298719, -0.591763] ; uv: [0.611900, 0.886700] ;
 		fscanf_s(NFG, "   %d. pos: [%f, %f, %f] ; norm: [%f, %f, %f] ; binorm: [%f, %f, %f] ; tgt: [%f, %f, %f] ; uv: [%f, %f] ;",
 			&baris, &vertex[a].x, &vertex[a].y, &vertex[a].z, &nrmx, &nrmy, &nrmz, &binrmx, &binrmy, &binrmz, &tgtx, &tgty, &tgtz, &vertex[a].u, &vertex[a].v);
 	}
 
-	fscanf_s(NFG, "NrIndices:%d", &nrindices);
-	for (int b = 1;b <= nrindices;b++)
+	fscanf_s(NFG, "\nNrIndices:%d\n", &nrindices);
+	for (int b = 0; b < (nrindices / 3); b++)
 	{
 		//0.    0, 1, 2
-		fscanf_s(NFG, "%d    %d, %d, %d", &baris, &index[b].dex1, &index[b].dex2, &index[b].dex3);
+		fscanf_s(NFG, "   %d.    %d,    %d,    %d", &baris, &index[b].dex1, &index[b].dex2, &index[b].dex3);
 	}
 
 }
