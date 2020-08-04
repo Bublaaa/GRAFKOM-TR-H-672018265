a49ce917928db68d6a9e7e2c7d29551b5bf927e0
Ggoober1 <48183327+Ggoober1@users.noreply.github.com>
2020-08-04 11:08:17 +0700

    Add files via upload

---
+++ TR_Grafkom/TR_Grafkom/vertex.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+struct ver
+{
+	float x, y, z, u, v;
+};
+
+struct indi
+{
+	int dex1, dex2, dex3;
+};
+
+ver* vertex = new ver[512];
+indi* index = new indi[717];
