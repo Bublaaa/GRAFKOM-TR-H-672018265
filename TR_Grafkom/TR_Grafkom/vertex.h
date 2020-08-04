33fb2d3dfbe105789d5b777e5d82c97ce0abd861
Bublaaa <66330115+Bublaaa@users.noreply.github.com>
2020-08-05 03:17:49 +0700

    Update vertex.h

--- TR_Grafkom/TR_Grafkom/vertex.h
+++ TR_Grafkom/TR_Grafkom/vertex.h
@@ -3,13 +3,21 @@
 
 struct ver
 {
-	float x, y, z, u, v;
+	GLfloat x, y, z, u, v;
 };
 
 struct indi
 {
-	int dex1, dex2, dex3;
+	GLint dex1, dex2, dex3;
 };
 
 ver* vertex = new ver[512];
 indi* index = new indi[718];
+
+struct pixel {
+	uint32_t colors;
+	struct
+	{
+		uint8_t R, G, B;
+	};
+};
