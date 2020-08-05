c3f9acbde974a99b0c1ceca6af4fbfdbf343516f
YIP <ts.indra10@gmail.com>
2020-08-05 21:22:03 +0700

    Update nfgloader.h
    Fix Load tga file texture

--- TR_Grafkom/TR_Grafkom/nfgloader.h
+++ TR_Grafkom/TR_Grafkom/nfgloader.h
@@ -1,32 +1,37 @@
 #pragma once
-#include <iostream>
-#include "vertex.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include"vertex.h"
 
-int load(const char* path)
-{
-	float baris, nrmx, nrmy, nrmz, binrmx, binrmy, binrmz, tgtx, tgty, tgtz;
-	int nvertices, nrindices;
-	FILE* NFG;
-	fopen_s(&NFG, path, "r");
-	if (!NFG)
-	{
-		printf("Can't open the file !\n");
+GLuint textureID; 
+using namespace std;
+
+GLuint loadtga(const char* Path) {
+	pixel* pixelsData;
+	vector<uint8_t> pixels; 
+	uint32_t width, height, size, bitperpixel;
+	fstream inFile(Path, ios::in | ios::binary);
+
+	uint8_t header[18] = { 0 }; 
+
+	vector<uint8_t> colorData;
+	inFile.read(reinterpret_cast<char*>(&header), sizeof(header));
+	bitperpixel = header[16];
+	width = header[13] * 256 + header[12];
+	height = header[15] * 256 + header[14];
+	size = size = ((width * bitperpixel + 31) / 32) * 4 * height;
+
+	if ((bitperpixel != 24) && (bitperpixel != 32)) {
+		inFile.close();
+		printf("Not a TGA File");
 		return 0;
 	}
-
-	fscanf_s(NFG, "NrVertices:%d", &nvertices);
-	for (int a = 0; a < nvertices; a++)
-	{
-		//0. pos: [0.134000, 1.020300, -0.083900] ; norm: [0.662725, 0.317712, -0.678126] ; binorm: [0.014559, 0.899903, 0.435847] ; tgt: [-0.748721, 0.298719, -0.591763] ; uv: [0.611900, 0.886700] ;
-		fscanf_s(NFG, "   %d. pos: [%f, %f, %f] ; norm: [%f, %f, %f] ; binorm: [%f, %f, %f] ; tgt: [%f, %f, %f] ; uv: [%f, %f] ;",
-			&baris, &vertex[a].x, &vertex[a].y, &vertex[a].z, &nrmx, &nrmy, &nrmz, &binrmx, &binrmy, &binrmz, &tgtx, &tgty, &tgtz, &vertex[a].u, &vertex[a].v);
-	}
-
-	fscanf_s(NFG, "\nNrIndices:%d\n", &nrindices);
-	for (int b = 0; b < (nrindices / 3); b++)
-	{
-		//0.    0, 1, 2
-		fscanf_s(NFG, "   %d.    %d,    %d,    %d", &baris, &index[b].dex1, &index[b].dex2, &index[b].dex3);
-	}
-
+	colorData.resize(size);
+	inFile.read(reinterpret_cast<char*>(colorData.data()), size);
+	inFile.close();
+	pixels = colorData;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels.data()); 
 }
